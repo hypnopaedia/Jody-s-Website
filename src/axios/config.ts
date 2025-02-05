5f37c6af7c588357643554d084f7d69d99fe28e2
Jody Salani <jodysalani@gmail.com>
2025-02-05 18:19:00 -0500

    cleanup

--- src/axios/config.ts
+++ src/axios/config.ts
@@ -1,12 +1,10 @@
 import axios from "axios";
-
-export const BACKEND_URL = 'http://127.0.0.1:8000/api';
-// export const BACKEND_URL = '/api/';
+import { BACKEND_URL, PHRASE } from "./constants";
 
 export const backend = axios.create({
     baseURL: BACKEND_URL,
     timeout: 3000,
     headers: { 
-      phrase: "615d1ba6c1d047606e256dc6a41335d6ac19d89863b95eea2ce7237bfe9400cb",
+      phrase: PHRASE,
     }
 });
