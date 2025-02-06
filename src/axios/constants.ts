46d03c2dd0c5bd76c983873d33e42485e5b2bb3b
Jody Salani <jodysalani@gmail.com>
2025-02-05 21:29:01 -0500

    fix deployment url

--- src/axios/constants.ts
+++ src/axios/constants.ts
@@ -1,4 +1,4 @@
 export const BACKEND_URL = 'http://127.0.0.1:8000/api';
-// export const BACKEND_URL = '/api/';
+// export const BACKEND_URL = window.location.origin + '/api';
 
 export const PHRASE = "615d1ba6c1d047606e256dc6a41335d6ac19d89863b95eea2ce7237bfe9400cb";
