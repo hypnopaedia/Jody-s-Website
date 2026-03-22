c9d44dc26f78f43ea2baaa509fdd5e451cf85125
Jody Salani <jodysalani@gmail.com>
2026-03-22 16:37:32 -0400

    additional selection fixes

--- src/components/Main/Routes/PostPage/Title/BackButton/BackButton.tsx
+++ src/components/Main/Routes/PostPage/Title/BackButton/BackButton.tsx
@@ -1,9 +1,10 @@
+import clsx from 'clsx';
 import classes from './BackButton.module.scss';
 import { Link } from "react-router-dom";
 import { IconButton } from 'src/components/shared/Button/IconButton';
 
 export const BackButton = () => (
     <Link to="/">
-        <IconButton className={classes.back} additionalText='Back'>arrow_back_ios</IconButton>
+        <IconButton className={clsx(classes.back, 'no-select')} additionalText='Back'>arrow_back_ios</IconButton>
     </Link>
 );
